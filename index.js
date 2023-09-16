929c4878830c6d4537102f053ff7ab6d21daba7b
NickooMar <nicoo.marsili@gmail.com>
2023-09-15 21:55:33 -0300

    Billing page

--- index.js
+++ index.js
@@ -54,9 +54,11 @@ async function homePage(page) {
 }
 
 async function pointOfSalesPage(page) {
+  const billingAdress = "3";
+
   // Wait for the select element to become visible and press value 3
   await page.waitForSelector("#puntodeventa");
-  await page.select("#puntodeventa", "3");
+  await page.select("#puntodeventa", billingAdress);
 
   // Wait for the select element to become visible and press value FACTURA_B
   // By some reason the page uses a timeout of 350ms to fill the select with data
@@ -72,25 +74,89 @@ async function pointOfSalesPage(page) {
 
 async function emissionDates(page) {
   try {
-    await page.waitForSelector("#fc_btn");
-    await page.click("#fc_btn");
+    /* IT JUST KEEP THE DATE OF TODAY, I COULDNT MAKE THE SELECT DATE WORK */
+    // await page.waitForSelector("#fc_btn");
+    // await page.click("#fc_btn");
 
-    const desiredDate = "15"; // Adjust as needed
-    const dateCellSelector = `td.day.false:contains('${desiredDate}')`;
+    // const desiredDate = "18"; // Adjust as needed
+    // const dateCellSelector = `html body div.calendar table tbody tr.daysrow td.day.false:contains('${desiredDate}')`;
 
-    // Wait for the date cell to become visible
-    await page.waitForSelector(dateCellSelector);
+    // await page.waitForSelector(dateCellSelector);
+    // await page.click(dateCellSelector);
 
-    // Hover over the date cell to trigger the class change
-    await page.hover(dateCellSelector);
+    const PRODUCTS = "1"; // Producto
+    const PER_MINOR = "472111"; // Venta al por menor
 
-    // Click the date cell with the new class (day.false.hilite)
-    await page.waitForSelector(`${dateCellSelector}.hilite`);
-    await page.click(`${dateCellSelector}.hilite`);
+    const selectConcept = "select#idconcepto";
+    const optionConcept = PRODUCTS;
 
-    //   await page.click('input[value="Continuar >"][onclick="validarCampos();"]');
+    const selectTypeOfSale = "select#actiAsociadaId";
+    const optionTypeOfSale = PER_MINOR;
+
+    // Concept of sale
+    await page.waitForSelector(selectConcept);
+    await page.select(selectConcept, optionConcept);
+
+    // Type of sale
+    await page.waitForSelector(selectTypeOfSale);
+    await page.select(selectTypeOfSale, optionTypeOfSale);
+
+    const continuarButtonSelector = 'input[value="Continuar >"]';
+
+    await page.waitForSelector(continuarButtonSelector);
+    await page.click(continuarButtonSelector);
+
+    await receptorInformationPage(page);
   } catch (error) {
     console.log(error);
+  }
+}
+
+async function receptorInformationPage(page) {
+  const selectIVACondition = "select#idivareceptor";
+  const IVA_CONDITION = "5"; // Consumidor Final
+
+  await page.waitForSelector(selectIVACondition);
+  await page.select(selectIVACondition, IVA_CONDITION);
+
+  const checkboxSelector = "input#formadepago1"; // Contado
+
+  await page.waitForSelector(checkboxSelector);
+  await page.click(checkboxSelector);
+
+  const continuarButtonSelector = 'input[value="Continuar >"]';
+
+  await page.waitForSelector(continuarButtonSelector);
+  await page.click(continuarButtonSelector);
+
+  await billingPage(page);
+}
+
+async function billingPage(page) {
+  // First determine how many rows of products we want
+  const rowsQty = 4;
+  const addButtonSelector = 'input[value="Agregar línea descripción"]';
+
+  await page.waitForSelector(addButtonSelector);
+
+  for (let i = 1; i < rowsQty; i++) {
+    const textareaSelector = `#detalle_descripcion${i}`; // TextArea
+    const selectMeasurement = `#detalle_medida${i}`; // Select
+    // Select the option by its value and text content
+    const optionValue = "7";
+    const optionText = "unidades";
+    const dataToFill = "This is the data I want to fill the textarea with.";
+
+    await page.waitForSelector(textareaSelector);
+    await page.type(textareaSelector, dataToFill);
+
+    await page.waitForSelector(selectMeasurement);
+    await page.select(
+      selectMeasurement,
+      `option[label="${optionText}"]`
+    );
+
+    // await page.click(addButtonSelector);
   }
 }
 
